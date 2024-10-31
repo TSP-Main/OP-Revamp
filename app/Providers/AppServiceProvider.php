f27773c1422422c730adb19f312b1291e82abc4c
Developertsp <167227304+Developertsp@users.noreply.github.com>
2024-10-31 23:54:21 +0500

    storage path configure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->app->useStoragePath(config('app.storage_path'));
         Paginator::useBootstrap();
     }
 }
